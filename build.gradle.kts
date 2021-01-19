a748233ff8cfc34403ecd6cf3200af0f2e5ed5a6
wenhailin <564544234@qq.com>
2021-01-19 11:26:12 +0800

    dependency added

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 }
 
 dependencies {
+    implementation(group = "redis.clients", name = "jedis", version = "3.4.1")
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
--- src/main/kotlin/com/github/wenslo/redisinaction/Chapter1.kt
+++ src/main/kotlin/com/github/wenslo/redisinaction/Chapter1.kt
@@ -1,7 +1,17 @@
 package com.github.wenslo.redisinaction
 
+import redis.clients.jedis.Jedis
+
 /**
  * Redis In Action 第一章 代码
  */
 class Chapter1 {
+    fun run() {
+        val jedis = Jedis("127.0.0.1", 6379)
+    }
+}
+
+fun main() {
+    val chapter1 = Chapter1()
+    chapter1.run()
 }
