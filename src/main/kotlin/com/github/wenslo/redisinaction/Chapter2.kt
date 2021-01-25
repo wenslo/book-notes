dda332a6f5f46e3f5e2696c610fee746dfb082aa
wenhailin <564544234@qq.com>
2021-01-25 22:36:01 +0800

    code add

--- src/main/kotlin/com/github/wenslo/redisinaction/Chapter2.kt
+++ src/main/kotlin/com/github/wenslo/redisinaction/Chapter2.kt
@@ -1,6 +1,7 @@
 package com.github.wenslo.redisinaction
 
 import redis.clients.jedis.Jedis
+import kotlin.math.min
 
 class Chapter2 {
 
@@ -14,6 +15,7 @@ class Chapter2 {
         conn.select(15)
     }
 
+    /****************************************** 登录和cookie缓存 start**********************************/
     fun checkToken(conn: Jedis, token: String): String? {
         return conn.hget("login:", token)
     }
@@ -46,6 +48,59 @@ class Chapter2 {
             conn.zrem("recent:", *tokens.toTypedArray())
         }
     }
+    /****************************************** 登录和cookie缓存 end**********************************/
+
+    /****************************************** 购物车 start ****************************************/
+    fun addToCart(conn: Jedis, session: String, item: String, count: Long) {
+        if (count <= 0)
+            conn.hdel("cart:$session", item)
+        else
+            conn.hset("cart:$session", item, count.toString())
+    }
+
+    fun cleanFullSessions(conn: Jedis) {
+        while (!QUIT) {
+            val size = conn.zcard("recent:")
+            if (size <= LIMIT) {
+                Thread.sleep(1000)
+                continue
+            }
+            val endIndex = min(size - LIMIT, 100)
+            val sessions = conn.zrange("recent:", 0, endIndex - 1)
+            val sessionKeys = mutableListOf<String>()
+            for (sess in sessions) {
+                sessionKeys.add("viewed:$sess")
+                sessionKeys.add("cart:$sess")
+            }
+            conn.del(*sessionKeys.toTypedArray())
+            conn.hdel("login:", *sessions.toTypedArray())
+            conn.zrem("recent:", *sessions.toTypedArray())
+        }
+    }
+
+    fun cacheRequest(
+        conn: Jedis,
+        request: String,
+        callback: (request: String) -> String,
+        canCache: (conn: Jedis, request: String) -> Boolean
+    ): String {
+        // if not can cache
+        if (!canCache(conn, request)) {
+            return callback(request)
+        }
+        val pageKey = "cache:${request.hashCode()}"
+        var content = conn.get(pageKey)
+        if (content.isBlank()) {
+            content = callback(request)
+            conn.setex(pageKey, 300, content)
+        }
+        // set with expired
+        return content
+    }
+
+    /****************************************** 购物车 end **************************************/
+
+    /****************************************** 数据行缓存 start *********************************/
 }
 
 fun main() {
