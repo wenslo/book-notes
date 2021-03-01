c0e1e3e2855addc1bd860d3422220642b5feaf29
wenhailin <564544234@qq.com>
2021-03-01 14:09:33 +0800

    jvm测试添加

---
+++ src/main/java/com/github/wenslo/深入理解JVM/chapter2/HeapOOM.java
@@ -0,0 +1,19 @@
+package com.github.wenslo.深入理解JVM.chapter2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 堆内存溢出测试
+ */
+public class HeapOOM {
+    static class OOMObject {
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+}
---
+++ src/main/java/com/github/wenslo/深入理解JVM/chapter2/JavaVMStackSOF.java
@@ -0,0 +1,24 @@
+package com.github.wenslo.深入理解JVM.chapter2;
+
+/**
+ * -Xss 128k
+ */
+public class JavaVMStackSOF {
+    private int stackLength = 1;
+
+    private void stackLeak() {
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackSOF stackSOF = new JavaVMStackSOF();
+        try {
+            stackSOF.stackLeak();
+        } catch (Throwable t) {
+            System.out.println("stack length:" + stackSOF.stackLength);
+            throw t;
+        }
+    }
+
+}
